82c630cec908cfc46391ca4e2620757007193df0
Asmenys <76791232+Asmenys@users.noreply.github.com>
2021-10-11 18:03:19 +0300

    Added size, tail and at(index) methods to the LinkedList class

--- linkedlist.rb
+++ linkedlist.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require 'awesome_print'
 require 'pry-byebug'
 
@@ -24,17 +25,44 @@ class LinkedList
     temp_next_node_pointer = head.next_node unless head.next_node.nil?
     @head.next_node = Node.new(value, temp_next_node_pointer)
   end
+
+  def size(head = @head)
+    result = 0
+    if head.next_node.nil?
+      1
+    else
+      result = size(head.next_node)
+      result += 1
+      result
+    end
+  end
+
+  def tail(head = @head)
+    if head.next_node.nil?
+      head
+    else
+      tail(head.next_node)
+    end
+  end
+
+  def at(index, current_index = 0, head = @head)
+    if current_index == index
+      head
+    else
+      at(index, current_index += 1, head.next_node)
+    end
+  end
 end
 
-# node class 
+# node class
 class Node
   attr_accessor :value, :next_node
 
-  def initialize (value = nil, next_node = nil)
+  def initialize(value = nil, next_node = nil)
     @value = value
     @next_node = next_node
   end
 end
-tlist = LinkedList.new("head")
+tlist = LinkedList.new('head')
 binding.pry
 text = 'text'
